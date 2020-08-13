954d66950bd9bc4ca2164c8b28a07057ffed97d6
Andre Pierre <andre.laurence94@gmail.com>
2020-08-13 03:55:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,8 +34,15 @@ def monopoly_with_third_tier
 :rent_in_dollars = {
   :one_piece_owned => 25, 
   :two_pieces_owned => 50, 
-  :three_pieces_ownded => 200}
-:names => {:reading_railroad => {}, :pennslyvania_railroad => {}, :shortline_railroad => {}}
+  :three_pieces_ownded => 200
+  
+}
+
+:names => {:reading_railroad => {},
+:pennslyvania_railroad => {}, 
+:shortline_railroad => {}
+}
+
 end
 
 def monopoly_with_fourth_tier

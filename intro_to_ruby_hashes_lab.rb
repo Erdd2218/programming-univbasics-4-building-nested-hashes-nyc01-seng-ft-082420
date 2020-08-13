1fae138cf4f22d4663fb1a385be39b51c10c5155
Andre Pierre <andre.laurence94@gmail.com>
2020-08-13 03:52:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,11 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
    {
-    :railrods => 
-    {:pieces => 4, :rent_in_dollars => {}, :names => {}
+    :railrods => {
+    
+    :pieces => 4, 
+    :rent_in_dollars => {}, 
+    :names => {}
       
     }
   }

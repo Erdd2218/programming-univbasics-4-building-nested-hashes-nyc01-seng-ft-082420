cab0d2a6f385a03bbd6b5c733ff60ad52ff974e3
Andre Pierre <andre.laurence94@gmail.com>
2020-08-13 04:08:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,8 @@ railrods: {
   pieces: 4, 
   rent_in_dollars: {
   :one_piece_owned => 25, 
-  :two_pieces_owned => 50, 
+  :two_pieces_owned => 50,
+  :three_pieces_owned => 100,
   :three_pieces_ownded => 200
   
 }

67f71297ecec25331f7ea9d17d0c14353739d66f
Andre Pierre <andre.laurence94@gmail.com>
2020-08-13 03:57:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,7 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 :reading_railroad { :mortgage_value => 100 }
-:pennslyvania_railroad = :mortgage_value => 200 
-:shortline_railroad = :mortgage_value => 800
+:pennslyvania_railroad { :mortgage_value => 200 }
+:shortline_railroad { :mortgage_value => 800 }
 
 end
